b9b5b20367dcc400371ba012a726a7845013d772
nono <novar.ludovic@gmail.com>
2017-11-03 17:30:36 +0100

    launch game

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -8,8 +8,16 @@
 
 namespace App\Controller;
 
+use App\Game\GameContext;
+use App\Game\GameRunner;
+use App\Game\Loader\TextFileLoader;
+use App\Game\Loader\XmlFileLoader;
+use App\Game\WordList;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
+
 /**
  * @Route("/game")
  */
@@ -17,9 +25,27 @@ class GameController extends Controller {
     /**
      * @Route("/")
      */
-    public function game(){
+    public function game(Request $request){
         //dump(getenv(('APP_ENV')));
-        return $this->render('game/game.html.twig');
+        $session = $request->getSession();
+        $wordList = new WordList();
+        $wordList->addLoader('txt',new TextFileLoader());
+        $wordList->addLoader('xml',new XmlFileLoader());
+
+        $wordList->loadDictionaries([
+            __DIR__.'/../../data/test.txt',
+            __DIR__.'/../../data/words.txt',
+            __DIR__.'/../../data/words.xml',
+        ]);
+
+        $context = new GameContext($session);
+
+        $runner = new GameRunner($context,$wordList);
+
+
+        return $this->render('game/game.html.twig',[
+            'game' => $runner->loadGame(11),
+        ]);
     }
     /**
      * @Route("/letter/{letter}")
@@ -46,11 +72,17 @@ class GameController extends Controller {
         return $this->render('game/game.html.twig');
     }
 
+    /**
+     * @Route("/won")
+     */
     public function won(){
         //dump(getenv(('APP_ENV')));
         return $this->render('game/won.html.twig');
     }
 
+    /**
+     * @Route("/failed")
+     */
     public function failed(){
         //dump(getenv(('APP_ENV')));
         return $this->render('game/failed.html.twig');
